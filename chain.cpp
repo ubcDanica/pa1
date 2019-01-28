193ff9230e55da61db0d2fd801952a3bf378f78e
fishmankevinyu <kaikailongwang3@gmail.com>
2019-01-27 20:57:14 -0800

    debugged moveback

--- chain.cpp
+++ chain.cpp
@@ -28,13 +28,13 @@ void Chain::insertBack(const Block & ndata){
     head_->next = head_;
   }
 
-  else if(head_-> prev == head_ && head_ -> next == head_ && head_-> data == NULL){
+  else if(head_-> prev == head_ && head_ -> next == head_ && head_-> data.height() == NULL){
       head_ = new Node(ndata);
       head_->prev = head_;
       head_->next = head_;
   }
 
-  else if(head_-> prev == head_ && head_ -> next == head_ && head_-> data != NULL){
+  else if(head_-> prev == head_ && head_ -> next == head_ && head_-> data.height() != NULL){
       head_->prev = new Node(ndata);
       head_->prev->next = head_;
       head_->prev->prev = head_;
